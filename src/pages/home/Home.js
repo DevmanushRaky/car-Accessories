3178432ce9737a81657784bccdb873c63b4a2ed4
DevmanushRaky <rakesucluci@gmail.com>
2023-07-15 14:15:41 +0530

    designing home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Home.css"
+import { Link } from 'react-router-dom';
 import Helmet from '../../components/helmet/Helmet'
 import Carousel from 'react-bootstrap/Carousel';
 import Car1 from "../../assests/cars1.jpg"
@@ -11,6 +12,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import CarBodyCover from "../../assests/carbodycover.jpg"
+import Button from 'react-bootstrap/Button';
 
 function Home() {
   return <Helmet title={"Home"}>
@@ -68,45 +71,46 @@ function Home() {
           <h4 className='our-product-heading'> Our Products</h4>
           <Row>
             <Col>
-              <Card >
-                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+              <Card>
+                <Card.Img variant="top" src={CarBodyCover} />
                 <Card.Body>
-                  <Card.Title>Top Hood</Card.Title>
+                  <Card.Title>Top Hood Covers</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    Choose from a variety of top hood covers to protect your car's hood
+                    in style.
                   </Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                  <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                  <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                  <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                  <ListGroup.Item>High-quality materials for durability</ListGroup.Item>
+                  <ListGroup.Item>Weather-resistant and UV protection</ListGroup.Item>
+                  <ListGroup.Item>Easy installation and secure fit</ListGroup.Item>
                 </ListGroup>
                 <Card.Body>
-                  <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link>
+                  <Link to="/products">
+                    <Button variant="warning">Full Details</Button>
+                  </Link>
                 </Card.Body>
               </Card>
             </Col>
 
             <Col>
-              <Card >
-                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+              <Card>
+                <Card.Img variant="top" src={CarBodyCover} />
                 <Card.Body>
-                  <Card.Title>Body Covers</Card.Title>
+                  <Card.Title>Car Body Covers</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    Keep your car protected and looking pristine with our high-quality car body covers.
                   </Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                  <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                  <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                  <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                  <ListGroup.Item>Full coverage for maximum protection</ListGroup.Item>
+                  <ListGroup.Item>Waterproof and resistant to UV rays</ListGroup.Item>
+                  <ListGroup.Item>Soft inner lining to prevent scratches</ListGroup.Item>
                 </ListGroup>
                 <Card.Body>
-                  <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link>
+                  <Link to="/products">
+                    <Button variant="warning">Full Details</Button>
+                  </Link>
                 </Card.Body>
               </Card>
             </Col>
@@ -114,15 +118,14 @@ function Home() {
 
 
           <Row>
-            <h4> Customer Testonomials </h4>
+            <h4 className='our-product-heading'> Customer Testonomials </h4>
             <Col>
               <Card >
                 <Card.Body>
-                  <Card.Title>Customer 1</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                  <Card.Title> Adarsh Srivastava </Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Delhi</Card.Subtitle>
                   <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    "Absolutely love the website and shop! I recently purchased car top hood covers and car body covers from them, and I couldn't be happier with my purchase. The website is user-friendly and provides all the necessary information about the products.
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -131,28 +134,37 @@ function Home() {
             <Col>
               <Card>
                 <Card.Body>
-                  <Card.Title>Customer 2</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                  <Card.Title> Rakesh Yadav </Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Lucknow</Card.Subtitle>
                   <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    The car top hood covers I bought are fantastic. They fit my car perfectly and provide excellent protection against the elements. The covers are made of high-quality materials, and the waterproof feature has proven to be extremely effective during rainy days.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title> Abhishek Patel </Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Lucknow</Card.Subtitle>
+                  <Card.Text>
+                    The car body covers are equally impressive. They provide full coverage for my vehicle and have a soft inner lining that prevents any scratches or damage. I appreciate the durability of these covers, as they have withstood various weather conditions.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title> Jordi Singh </Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Lucknow</Card.Subtitle>
+                  <Card.Text>
+                    Feel free to customize this review according to your specific product offerings and customer experiences. It's important to highlight the quality of your products, the user-friendly website, prompt delivery, and excellent customer service.
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
 
-            <Col>
-              <Card >
-                <Card.Body>
-                  <Card.Title>Customer 3</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
 
           </Row>
         </Container>
